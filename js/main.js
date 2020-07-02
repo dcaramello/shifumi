bd822dbbb21727f52d1682efe2aab56d8358548b
Dim <dcaramello@gmail.com>
2020-07-02 15:43:52 +0200

    add allow to enter and store the name

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 //  ...~~~*** Jeux du Shifumi ***~~~... //
- 
-alert("Bonjour\nUne petite partie de Shifumi ?\ntaper sur OK !");
+
+// Welcome message
+alert("Bienvenue\nUne petite partie de Shifumi ?\ntaper sur OK !");
+
+// Allow to enter and store the name
+var name = prompt("Rentrez votre nom");
+alert("Bonjour " + name);
 
 
 
