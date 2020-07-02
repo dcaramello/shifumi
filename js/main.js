d73d3a7cba1e2e13519b739a8b09663ddd165c2f
Dim <dcaramello@gmail.com>
2020-07-02 20:01:20 +0200

    create a function for computer

--- js/main.js
+++ js/main.js
@@ -12,35 +12,37 @@ var userValue = prompt("Bonjour " + name + " \n\npierre , feuille  ou  ciseaux ?
 
 // L'ordinateur
 
-
-
-
 // la fonction getRandom permet de générer un entier aléatoire dans une intervalle entre 0(inclus) et 3(exclus)
 // la fonction Math.floor renvoi un entier
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  
-pcValue = ["pierre", "feuille", "ciseaux"];
 
+
+var pcValue = ["pierre", "feuille", "ciseaux"];
+
+function computerChoice(){
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+      
 var result = getRandomInt(3);
-  console.log(result);
+console.log(result);
+
+var pcChoice = "";
 
 if(result === 0) {
     console.log("L'ordinateur à choisi " + pcValue["0"]);
 }
-
 else if(result === 1) {
     console.log("L'ordinateur à choisi " + pcValue["1"]);
 }
-
 else{
     console.log("L'ordinateur à choisi " + pcValue["2"]);
 }
+return pcChoice;
+}
 
-
-
-
+pcChoice = computerChoice(pcValue);
+computerChoice;
 
 
 
